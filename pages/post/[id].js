033538c1b4c347f153d97cc5f796afe89b9f633a
Rodrigo Lima Cortes <69279039+MalfCortes@users.noreply.github.com>
2021-04-19 22:05:22 -0300

    Update [id].js

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -29,7 +29,7 @@ export default function PostItens(){
 }
 
 export async function getServerSideProps(){
-  const res = await fetch('http://localhost:3000/api/trending');
+  const res = await fetch('https://buscador-filmes-malfcortes.vercel.app//api/trending');
   const json = await res.json();
   return{
       props:{
