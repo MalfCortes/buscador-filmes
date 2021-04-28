3b399fac853fea09924c3df6d493fed6672af73f
Rodrigo Lima Cortes <69279039+MalfCortes@users.noreply.github.com>
2021-04-27 22:20:09 -0300

    Update [id].js

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -36,7 +36,7 @@ export default function MovieItem({info}){
 
 
 export async function getServerSideProps(context){
-  const res = await fetch(`http://localhost:3000/api/movie/${context.params.id}`);
+  const res = await fetch(`https://buscador-filmes-malfcortes.vercel.app//api/movie/${context.params.id}`);
   const json = await res.json();
   return{
       props:{
